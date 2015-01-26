6a09dba89d71b2e46f30f8ff7a9b7bf9e58894e1
matlink <matlink@matlink.fr>
2015-01-26 17:57:41 +0100

    testing modification

--- pgcd/pelicanconf.py
+++ pgcd/pelicanconf.py
@@ -23,6 +23,7 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
+
 #LINKS = (('Pelican', 'http://getpelican.com/'),
 #         ('Python.org', 'http://python.org/'),
 #         ('Jinja2', 'http://jinja.pocoo.org/'),
